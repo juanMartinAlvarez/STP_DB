974bbc51ecf0adb381def8e64f4384ed30ff6feb
juanMartinAlvarez <jalvarez@citedef.gob.ar>
2022-05-18 22:56:03 -0300

    refact

--- DB/DB.py
+++ DB/DB.py
@@ -1,13 +1,14 @@
 from PySide2 import QtCore, QtGui, QtWidgets
 from PySide2.QtUiTools import QUiLoader
-import DB.listaDeUsuarios as usuarios
+import DB.listaDeUsuarios as mostrar
 import DB.backUp as backUp
-import DB.perfil as perfil
-import DB.registro as registro
+import DB.perfilDeUsuario as editar
+import DB.registro as login
 
 class DB():
     def __init__(self, parent):
         self.parent = parent
+        #-- pyside settings
         #--- Set Vertical Box
         self.parent.VBox.addWidget(self.parent.DB_widget,5)
         self.parent.VBox.setContentsMargins(0, 0, 0, 0)
@@ -15,11 +16,13 @@ class DB():
         self.parent.setLayout(self.parent.VBox)
         self.parent.setFixedWidth(1360)
         self.parent.setFixedHeight(680)
-        #-Initialize classes listaDeUsuarios BackUp perfil registro 
-        self.usuarios = usuarios.listaDeUsuarios(self.parent.DB_ui)
+
+        #-Initialize classes
+        self.usuarios = mostrar.listaDeUsuarios(self.parent.DB_ui)
+        self.registro = login.registroDeUsuario(self.parent.DB_ui)
+        
+        self.perfil = editar.perfilDeUsuario(self.parent.DB_ui)
         self.bkUp = backUp.backUp(self.parent.DB_ui)
-        self.perfil = perfil.perfilDeUsuario(self.parent.DB_ui)
-        self.registro = registro.registroDeUsuario(self.parent.DB_ui)
 
     def listaDeUsuarios(self):
         self.usuarios.buttonDown(True)
@@ -27,20 +30,19 @@ class DB():
         listaDeUsuarios = self.usuarios.NombreDeUsuarios()
         listaDeDni = self.usuarios.Dni()
         self.usuarios.userUiShow(listaDeUsuarios, listaDeDni)
-
+   
+    def registrarUsusario(self):
+        self.registro.buttonDown(True)
+        self.registro.registroUiShow()
+    
+    def editarPerfil(self):
+        self.perfil.buttonDown(True)
+        self.perfil.perfilUiShow()
 
     def backUp(self):
         self.bkUp.buttonDown(True)
         self.bkUp.bkUpUiShow()
 
-    def editarPerfil(self):
-        self.perfil.buttonDown(True)
-        self.perfil.perfilUiShow()
-        
-    def registrarUsusario(self):
-        self.registro.buttonDown(True)
-        self.registro.registroUiShow()
-        
     def salir(self):
         #-Reset butttons Main Menu        
         self.parent.main_ui.AlineacionButton.setEnabled(True)
@@ -56,6 +58,4 @@ class DB():
         self.parent.DB_widget.setParent(None)
         #-Fix window
         self.parent.setFixedHeight(100)
-        
-
         
